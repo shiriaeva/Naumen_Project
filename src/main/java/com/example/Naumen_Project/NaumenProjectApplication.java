2fe12329fa0f627764438b9f7279c52d006564c0
shiriaeva <Elena.Shiriaeva@urfu.me>
2023-03-27 11:33:53 +0500

    Initial commit

---
+++ src/main/java/com/example/Naumen_Project/NaumenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Naumen_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaumenProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaumenProjectApplication.class, args);
+	}
+
+}
